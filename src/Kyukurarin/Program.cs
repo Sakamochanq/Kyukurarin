ee74cd06b546677f90fc1b4be5d2ecd9f5b57b62
Sakamochanq <sakamochan123@outlook.jp>
2025-09-04 17:35:29 +0900

    feat: NAudioでWavの再生

--- src/Kyukurarin/Program.cs
+++ src/Kyukurarin/Program.cs
@@ -1,9 +1,35 @@
-﻿using System;
+﻿using System.Threading;
+using NAudio.Wave;
+using System;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello Kyukurarin"); 
+        //Console.WriteLine("Hello Kyukurarin");
+
+        string audioPath = "./assets/audio/Kyukurarin.wav";
+
+        try
+        {
+            using (var afr = new AudioFileReader(audioPath))
+            using (var outputDevice = new WaveOutEvent())
+            {
+                outputDevice.Init(afr);
+                outputDevice.Play();
+
+                Console.WriteLine("Playing audio...");
+
+                //再生終了まで待機
+                while (outputDevice.PlaybackState == PlaybackState.Playing)
+                {
+                    Thread.Sleep(100);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
